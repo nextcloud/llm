96c4cc4f9d127142993d2f3270c283bd78d0965f
jld3103 <jld3103yt@gmail.com>
2023-11-07 10:29:35 +0100

    feat: Add leo hessianai model
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- src/components/ViewAdmin.vue
+++ src/components/ViewAdmin.vue
@@ -47,6 +47,12 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 					@update:checked="onChange">
 					{{ 'GPT4All Falcon' }}
 				</NcCheckboxRadioSwitch>
+				<NcCheckboxRadioSwitch :checked.sync="settings['model']"
+					type="radio"
+					value="leo-hessianai"
+					@update:checked="onChange">
+					{{ 'Leo Hessianai (' + t('llm', 'Bilingual: English/German') + ')' }}
+				</NcCheckboxRadioSwitch>
 			</p>
 			<p>{{ t('llm', 'To download the machine learning model, you need to excecute the occ command line interface of Nextcloud on your server terminal with the following command:') }}</p>
 			<p><code>occ llm:download-model {{ settings['model'] }}</code></p>
